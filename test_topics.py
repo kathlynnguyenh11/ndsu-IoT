c8fed3634ae3f8f37a4a9dcaa4a87da2c441f76b
Kathlyn Nguyen <kathlyn.hang.nguyen11@gmail.com>
2021-01-14 23:18:11 -0500

    rename topics

---
+++ test_topics.py
@@ -0,0 +1,34 @@
+from kafka import KafkaConsumer
+from pyspark import SparkContest
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+import simplejson as json
+
+KAFKA_TOPICS = "solar-module-raw"
+KAFKA_BROKERS = "localhost:9092"
+ZOOKEEPER = "localhost:2181"
+
+OUTPUT = "spark_out"
+
+sc = SparkContext.getOrCreate()
+
+ssc = StreamingContext(sc,60)
+kafkaStream = KafkaUtils.createStream(ssc, ZOOKEEPER, "spark-streaming", {KAFKA_TOPIC:1})
+
+def get_power(data):
+	#data = json.loads(data)
+	return data["power"]
+
+def calculate(data):
+	data = json.loads(data)
+	return data["powers"]*2
+
+#def handler(message):
+#	records = message.collect()
+	
+lines = kafkaStream.map(lambda x: "Initial value: {}, New value: {}".format(get_power(x[1]), calculate(x[1])))
+lines.pprint()
+
+ssc.start()
+ssc.awaitTermination()
+
