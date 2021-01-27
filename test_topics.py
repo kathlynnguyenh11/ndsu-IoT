3540b24fdfa122f724321060ba1fe31e007d6cf9
Kathlyn Nguyen <kathlyn.hang.nguyen11@gmail.com>
2021-01-27 00:25:32 -0500

    Check output

--- test_topics.py
+++ test_topics.py
@@ -36,7 +36,7 @@ def handler(message):
 		producer.flush()
 
 def main():
-	lines = kafkaStream.map(lambda x: x[1])
+	lines = kafkaStream.map(lambda x: get_type(x[1]))
 	lines.pprint()
 	#lines = kafkaStream.map(lambda x: "old value: {}".format(get_type(x[1])))
 	#lines = kafkaStream.map(lambda x: "Initial value: {}, New value: {}".format(get_type(x[1]), calculate(x[1])))
